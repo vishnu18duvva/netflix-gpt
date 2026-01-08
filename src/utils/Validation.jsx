cf7462b47c30ec1233538539c6dacc9905d84cc6
vishnu18duvva <vnduvva1804@gmail.com>
2026-01-08 20:40:29 +0530

    Update validation to match UI requirements

--- src/utils/Validation.jsx
+++ src/utils/Validation.jsx
@@ -1,12 +1,15 @@
 const checkValidateFormData = (email, password, name) => {
-    const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
-    const isPasswordValid = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/.test(password);
-    const isFullNameValid = /^[A-Za-z\s]{2,50}$/.test(name);
+  const isEmailValid = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+  const isPasswordValid =
+    /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&#])[A-Za-z\d@$!%*?&#]{10,}$/.test(password);
+  const isFullNameValid = /^[A-Za-z\s]{2,50}$/.test(name);
 
-    if (!isEmailValid) return "Invalid email format.";
-    if (!isPasswordValid) return "Password must be 8 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.";
-    if (!isFullNameValid) return "Name must be 2-50 characters long and contain only letters and spaces.";
+  if (!isEmailValid) return "Invalid email format.";
+  if (!isPasswordValid)
+    return "Password must be 10 characters long and include at least one uppercase letter, one lowercase letter, one number, and one special character.";
+  if (!isFullNameValid)
+    return "Name must be 2-50 characters long and contain only letters and spaces.";
 
-    return null;
-}
+  return null;
+};
 export default checkValidateFormData;
